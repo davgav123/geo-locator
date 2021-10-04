70e182a5ed44674f2fa4a371f94f16bd0d7bc3d6
davgav123 <davidgavrilovic@hotmail.com>
2021-10-04 23:55:47 +0200

    add leaflet maps to frontend

---
+++ geo-frontend/geo-leaflet-map.js
@@ -0,0 +1,12 @@
+
+var mymap = L.map('mapid').setView([45, 15], 5);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZGF2Z2F2IiwiYSI6ImNrdTdmYjZqdTU3YXUyb25xaGhhMWlndDUifQ.GXEmjuG3NmH23uvFsOIw-g'
+}).addTo(mymap);
+
