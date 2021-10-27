2fb80ffb7f80d29394ad7a9b3a616ded2873f571
davgav123 <davidgavrilovic@hotmail.com>
2021-10-27 23:47:30 +0200

    add more options to selections in the frontend

--- geo-frontend/geo-script.js
+++ geo-frontend/geo-script.js
@@ -1,5 +1,5 @@
 
-var displayMap = L.map('mapid').setView([45, 15], 5);
+var displayMap = L.map('mapid').setView([55, 15], 4);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
